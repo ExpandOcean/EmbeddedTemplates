bd67fa4beb56bcffdcaac51e0572b7edcbdd821b
freeyourmind <freeyourmind@126.com>
2014-09-20 20:26:59 +0800

    add STM32F4+Eclipse+semihosting+openocd+stllink

---
+++ STM32/src/main.c
@@ -0,0 +1,120 @@
+//
+// This file is part of the GNU ARM Eclipse distribution.
+// Copyright (c) 2014 Liviu Ionescu.
+//
+
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include "diag/Trace.h"
+
+#include "Timer.h"
+#include "BlinkLed.h"
+
+// ----------------------------------------------------------------------------
+//
+// Semihosting STM32F4 led blink sample (trace via STDOUT).
+//
+// In debug configurations, demonstrate how to print a greeting message
+// on the trace device. In release configurations the message is
+// simply discarded.
+//
+// To demonstrate semihosting, display a message on the standard output
+// and another message on the standard error.
+//
+// Then demonstrates how to blink a led with 1Hz, using a
+// continuous loop and SysTick delays.
+//
+// On DEBUG, the uptime in seconds is also displayed on the trace device.
+//
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the STDOUT output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace_impl.c
+// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
+
+// ----- Timing definitions -------------------------------------------------
+
+// Keep the LED on for 2/3 of a second.
+#define BLINK_ON_TICKS  (TIMER_FREQUENCY_HZ * 2 / 3)
+#define BLINK_OFF_TICKS (TIMER_FREQUENCY_HZ - BLINK_ON_TICKS)
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+char buf[100];
+
+int test_read_file_from_host(char * path)
+{
+	int res;
+	int filehdl;
+	filehdl=_open(path,0);
+	if(filehdl)
+	{
+		printf("open file ok");
+		res=_read(filehdl,buf,100);
+		printf(buf);
+		res=_close(filehdl);
+	}
+}
+
+int
+main(int argc, char* argv[])
+{
+
+  int x;
+	// Show the program parameters (passed via semihosting).
+  // Output is via the semihosting output channel.
+  trace_dump_args(argc, argv);
+
+  // Send a greeting to the trace device (skipped on Release).
+  trace_puts("Hello ARM World!");
+
+  // Send a message to the standard output.
+  puts("Standard output message.");
+
+  // Send a message to the standard error.
+  fprintf(stderr, "Standard error message.\n");
+
+  // At this stage the system clock should have already been configured
+  // at high speed.
+  trace_printf("System clock: %uHz\n", SystemCoreClock);
+
+  timer_start();
+
+  blink_led_init();
+  
+  uint32_t seconds = 0;
+
+  printf("input x\r\n");
+  scanf("%d",&x);
+  printf("%d",x);
+
+  test_read_file_from_host("d:/test.txt");
+  // Infinite loop
+  while (1)
+    {
+      blink_led_on();
+      timer_sleep(BLINK_ON_TICKS);
+
+      blink_led_off();
+      timer_sleep(BLINK_OFF_TICKS);
+
+      ++seconds;
+
+      // Count seconds on the trace device.
+      trace_printf("Second %u\n", seconds);
+    }
+  // Infinite loop, never return.
+}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
